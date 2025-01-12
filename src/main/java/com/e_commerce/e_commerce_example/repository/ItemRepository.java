fb1c270aea5a420cfaad790152d75a5bd2a8e658
uhyeon-macbookpro <wisebuddyu@gmail.com>
2025-01-13 00:04:46 +0900

    other test-s

--- src/main/java/com/e_commerce/e_commerce_example/repository/ItemRepository.java
+++ src/main/java/com/e_commerce/e_commerce_example/repository/ItemRepository.java
@@ -3,5 +3,11 @@ package com.e_commerce.e_commerce_example.repository;
 import com.e_commerce.e_commerce_example.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByPriceLessThan(Integer price);
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
