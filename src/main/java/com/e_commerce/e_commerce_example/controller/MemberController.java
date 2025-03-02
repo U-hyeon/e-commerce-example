1e29f83905e123bfb33a25fb008f73301bfdec31
U-hyeon <wisebuddyu@gmail.com>
2025-03-02 20:40:26 +0900

    회원가입구현

--- src/main/java/com/e_commerce/e_commerce_example/controller/MemberController.java
+++ src/main/java/com/e_commerce/e_commerce_example/controller/MemberController.java
@@ -3,9 +3,11 @@ package com.e_commerce.e_commerce_example.controller;
 import com.e_commerce.e_commerce_example.dto.MemberFormDto;
 import com.e_commerce.e_commerce_example.entity.Member;
 import com.e_commerce.e_commerce_example.service.MemberService;
+import jakarta.validation.Valid;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,16 +26,24 @@ public class MemberController {
     @GetMapping("/new")
     public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
-        model.addAttribute("errorMessage","");
         return "member/memberForm";
     }
 
-    @PostMapping
-    public String memberForm(MemberFormDto memberFormDto) {
-        // 회원가입 요청 정보로부터 비밀번호 암호화
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        // 회원가입 진행
-        memberService.saveMember(member);
+    @PostMapping("/new")
+    public String memberForm(@Valid MemberFormDto memberFormDto,
+                             BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
         // 메인페이지로 이동
         return "redirect:/";
     }
--- src/main/java/com/e_commerce/e_commerce_example/dto/MemberFormDto.java
+++ src/main/java/com/e_commerce/e_commerce_example/dto/MemberFormDto.java
@@ -1,7 +1,10 @@
 package com.e_commerce.e_commerce_example.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * <sl>
@@ -15,21 +18,27 @@ public class MemberFormDto {
     /**
      * 회원명
      */
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
     /**
      * 이메일
      */
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message="이메일 형식으로 입력해주세요.")
     private String email;
 
     /**
      * 비밀번호
      */
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=8, max=16, message="비밀번호는 8~16자 이어야 합니다.")
     private String password;
 
     /**
      * 주소
      */
+    @NotBlank(message = "주소는 필수 입력 값입니다.")
     private String address;
 
     public String getName() {
