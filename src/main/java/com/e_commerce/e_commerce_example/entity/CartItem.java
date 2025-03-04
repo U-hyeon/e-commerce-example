022ecd307e2c360fa84016c91b81aa8300883242
U-hyeon <wisebuddyu@gmail.com>
2025-03-04 22:49:30 +0900

    entity mapping. many-to-one

---
+++ src/main/java/com/e_commerce/e_commerce_example/entity/CartItem.java
@@ -0,0 +1,57 @@
+package com.e_commerce.e_commerce_example.entity;
+
+import jakarta.persistence.*;
+
+/**
+ * Cart와 Item 다대다 매핑을 보조하기 위한 엔티티
+ */
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
